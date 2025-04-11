1f185b966a1fa5dddcbd27205f42775bcdb6b017
mizandarii <kristiina.parri@ivkhk.ee>
2025-04-11 13:30:17 +0300

    1

--- inc/database.php
+++ inc/database.php
@@ -11,7 +11,7 @@ class database {
         $this->host = 'localhost';
         $this->user = 'root';
         $this->password = '';
-        $this->basename = 'newsportal';
+        $this->basename = 'bobarbr';
         $this->connect();
     }
 
